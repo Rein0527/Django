9f67b685b225deb42a22e2b6052b89d27498c6b4
Rein0527 <108525337+Rein0527@users.noreply.github.com>
2023-01-03 05:32:20 +0800

    Delete django_form directory

--- django_form/customer/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Customer
-# Register your models here.
-
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'email', 'tel')
-
-
-admin.site.register(Customer, CustomerAdmin)
--- django_form/customer/models.py
+++
@@ -1,8 +0,0 @@
-from unittest.util import _MAX_LENGTH
-from django.db import models
-
-# Create your models here.
-class Customer(models.Model):
-    name = models.CharField(max_length=30, blank=False, null=False)
-    email = models.EmailField(blank=False, null=False)
-    tel = models.IntegerField()
